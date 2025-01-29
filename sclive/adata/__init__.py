2939454b8ecdd6abf8bdcaf29284641388a9fd54
acihanckr <cakar.adnancihan@gmail.com>
2025-01-29 02:10:45 +0000

    adds get_gene_exprs

--- sclive/adata/__init__.py
+++ sclive/adata/__init__.py
@@ -1,1 +1,2 @@
 from .get_metas_func import get_metas
+from .get_gene_exprs_func import get_gene_exprs
---
+++ tests/adata/get_gene_exprs_test.py
@@ -0,0 +1,10 @@
+from sclive.adata.get_gene_exprs_func import get_gene_exprs
+from scanpy import read_h5ad
+
+def test_get_gene_exprs():
+    adata = read_h5ad("test_data/pbmc.h5ad")
+    adata_meta = get_gene_exprs(adata, genes=["SRM", "SIK1"])
+    adata_meta = get_gene_exprs(adata, genes=["SRM", "SIK1"], use_raw=True)
+    adata_meta = get_gene_exprs(adata, genes=["SRM", "SIK1"], use_raw=False)
+    adata_meta = get_gene_exprs(adata, genes=["SRM", "SIK1"], layer="counts", use_raw=True)
+    adata_meta = get_gene_exprs(adata, genes=["SRM", "SIK1"], layer="counts")
