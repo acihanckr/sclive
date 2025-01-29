47a939d83539847fc7be68839c43f09f8458413e
acihanckr <cakar.adnancihan@gmail.com>
2025-01-29 02:55:58 +0000

    adds get_dimred_with_exprs function

---
+++ tests/adata/get_dimred_with_exprs_test.py
@@ -0,0 +1,8 @@
+from sclive.adata.get_dimred_with_exprs_func import get_dimred_with_exprs
+from scanpy import read_h5ad
+
+def test_get_dimred_with_exprs():
+    adata = read_h5ad("test_data/pbmc.h5ad")
+    adata_meta = get_dimred_with_exprs(adata, dimred_id = "umap", comps=[0,1], dimred_id_suffix="X_", genes=["SRM", "SIK1"])
+    adata_meta = get_dimred_with_exprs(adata, dimred_id = "umap", comps=[0,1], dimred_id_suffix="X_", genes=["SRM", "FAKE1"])
+    adata_meta = get_dimred_with_exprs(adata, dimred_id = "umap", comps=[0,1], dimred_id_suffix="X_", genes=["FAKE2", "FAKE1"])
