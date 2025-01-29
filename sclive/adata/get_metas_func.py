112d56bf390c96c25408df33a04084887a8bf11c
acihanckr <cakar.adnancihan@gmail.com>
2025-01-29 01:56:10 +0000

    adds documentation

--- sclive/adata/get_metas_func.py
+++ sclive/adata/get_metas_func.py
@@ -1,17 +1,24 @@
+from typing import List, Optional
 import pandas as pd
 import polars as pl
-
-def get_metas(adata, meta_ids, cat=None):
+from anndata import AnnData
+def get_metas(adata:AnnData, 
+              meta_ids:List[str], 
+              cat:Optional[bool]=None):
     '''
     Extract meta data from AnnData object and return as Polars DataFrame
     
-    :param adata: AnnData object to extract meta data from    
-    :param meta_ids: List of meta data ids to export from Annotated data
-    :param cat: If True, then the meta data is assumed categorical, if false it is assumed numeric, if None, then the function will try to infer the type using pandas api
+    :param adata: 
+        AnnData object to extract meta data from    
+    :param meta_ids: 
+        List of meta data ids to export from Annotated data
+    :param cat: 
+        If True, then the meta data is assumed categorical, if false it is assumed numeric, if None, then the function will try to infer the type using pandas api
 
     Returns: 
     --------
-    Polars DataFrame with meta data and barcodes
+    polars.DataFrame: 
+        DataFrame with meta data and barcodes
     '''
     if cat is None:
         dtys = [pd.api.types.is_numeric_dtype(adata.obs[meta_id]) for meta_id in meta_ids] 
