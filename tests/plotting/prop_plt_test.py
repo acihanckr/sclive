a5e8f885d8ab4739e7ae0be4757a2e49cd79e393
acihanckr <cakar.adnancihan@gmail.com>
2025-03-10 22:02:36 +0000

    adds proportional plot funciton

---
+++ tests/plotting/prop_plt_test.py
@@ -0,0 +1,16 @@
+from anndata import read_h5ad
+from sclive.plotting.prop_plt_func import prop_plt
+
+adata = read_h5ad("test_data/pbmc.h5ad")
+
+def test_prop_plt(scanpy_std_pipeline):
+    prop_plt(adata, "leiden", "leiden", plt_type="pct").write_html("fig.html")
+    prop_plt(adata, "leiden", "fake_cats", plt_type="pct").write_html("fig.html")
+    prop_plt(adata, "fake_dogs", "fake_dogs", plt_type="count").write_html("fig.html")
+    prop_plt(adata, "leiden", "fake_cats", plt_type="count").write_html("fig.html")
+    prop_plt(adata, "leiden", "fake_cats", plt_type="count", coord_flip=True, axis_font_size=None).write_html("fig.html")
+    prop_plt(adata, "leiden", "fake_cats", plt_type="count", width = 400).write_html("fig.html")
+    prop_plt(adata, "leiden", "fake_cats", plt_type="count", width = 400, height= 80).write_html("fig.html")
+    prop_plt(adata, "leiden", "fake_cats", plt_type="count", axis_font_size=None).write_html("fig.html")
+    prop_plt(adata, "leiden", "fake_cats", plt_type="count", axis_font_size=12, title_size=12).write_html("fig.html")
+    prop_plt(adata, "leiden", "fake_cats", plt_type="count", axis_font_size=12, title_size=12, title="Fake Cats").write_html("fig.html")
